2e9ec6681a8b5c8d51425cf516545ea7a37a2ee2
Samuda789 <samimahamed5598@gmail.com>
2024-01-08 23:58:01 +0300

    add calculator

---
+++ calculation.py
@@ -0,0 +1,50 @@
+# I created 4 functions for addition, multiplication, substraction and division
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+# Displaying the 4 operations
+print("Select operation.\n1.Add(+)\n2.Subtract(-)\n3.Multiply(x)\n4.Divide(/)")
+
+# I created a while loop inorder to run the program for multiple times
+while True:
+    choice = input("Choose your operator(1,2,3,4): ")
+
+    if choice in ("1", "2", "3", "4"):
+        # the try function let's me check for errors in this block of code
+        # the except function let's me handle the error
+        # the ValueError will be raised if the user inputs a string in the num_1 or num_2
+        try:
+            num_1 = int(input("Enter first number: "))
+            num_2 = int(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. please enter a number.")
+            continue
+
+        if choice == "1":
+            print(num_1, "+", num_2, "=", add(num_1, num_2))
+        elif choice == "2":
+            print(num_1, "-", num_2, "=", subtract(num_1, num_2))
+        elif choice == "3":
+            print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+        elif choice == "4":
+            print(num_1, "/", num_2, "=", divide(num_1, num_2))
+
+        # created a new variable that decides whether to continue the programm or stop
+        next_calculation = input("Would you like to do another calculation(yes/no): ")
+        if next_calculation == "no":
+            break
+    else:
+        print("Invalid input")
